e0fc3ed271de3c9434d0971d5b356240d103ef51
tanmaysh17 <tanmay.sh17@gmail.com>
2016-07-21 10:38:58 +0530

    Adding p-value and hypothesis test

--- t-test.py
+++ t-test.py
@@ -9,6 +9,7 @@ tips = [20.8, 18.7, 19.1, 20.6, 21.9, 20.4, 22.8,
         19.2, 20.2, 21.1, 22.1, 21.0, 21.7];
 mu = 20
 m = np.mean(tips)
+alpha = 0.06
 
 plt.axis([18,22, 0.0, 1.6])
 x = np.arange(18, 23, 0.01)
@@ -20,5 +21,15 @@ plt.text(m, 0.1, "Sample mean = %0.2f" % m)
 # t-test
 
 t = (m - mu)/(np.std(tips, ddof=1)/pow(len(tips), 0.5))
-plt.text(m, 0.2, "t value = %0.2f" % t)
+p = (1-stats.t.cdf(t,len(tips)-1))
+tp = 2*p
+
+print "t is %f" % t
+print "one-sided p-value is %f" % p
+print "two-sided p-value is %f" % tp
+if p > alpha:
+    print "p-value (from t-test) = %f" % tp, ", Reject H1"
+else:
+    print "p-value (from t-test) = %f" % tp, ", Reject H0"
+
 plt.show()
